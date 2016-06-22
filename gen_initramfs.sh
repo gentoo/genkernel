764604a8c8cf24e90db28b657070f21f16dd1367
Jason Miller <aidenn0@geocities.com>
2016-06-22 16:12:10 -0700

    Fix little-endian problem with hostid

--- gen_initramfs.sh
+++ gen_initramfs.sh
@@ -100,7 +100,14 @@ append_base_layout() {
 	date -u '+%Y%m%d-%H%M%S' > ${TEMP}/initramfs-base-temp/etc/build_date
 	echo "Genkernel $GK_V" > ${TEMP}/initramfs-base-temp/etc/build_id
 
-	printf "$(hostid | sed 's/\([0-9A-F]\{2\}\)/\\x\1/gI')" > ${TEMP}/initramfs-base-temp/etc/hostid
+
+	if file -L "${TEMP}/initramfs-base-temp/bin/sh" \
+	    | grep -q 'MSB executable'; then
+	    hostid="$(hostid)"
+	else
+	    hostid="$(hostid | sed -E 's/(..)(..)(..)(..)/\4\3\2\1/')"
+	fi
+	printf "$(echo "${hostid}" | sed 's/\([0-9A-F]\{2\}\)/\\x\1/gI')" > ${TEMP}/initramfs-base-temp/etc/hostid
 
 	cd "${TEMP}/initramfs-base-temp/"
 	log_future_cpio_content
